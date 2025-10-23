9cb6eb6b7997eab4b25d1b8c9ea8833e64aadc82
s1140315-dev <s1140315@dmhs.kh.edu.tw>
2025-10-23 09:58:13 +0800

    Add files via upload

---
+++ HW3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "1. 快樂星球" << endl;
+    std::cout << "2. 快樂星球" << endl;
+    std::cout << "3. 快樂星球" << endl;
+    std::cout << "4. 快樂星球" << endl;
+    std::cout << "5. 快樂星球" << endl;
+    std::cout << "6. 快樂星球" << endl;
+    std::cout << "7. 快樂星球" << endl;
+
+    return 0;
+}
